e9f7ebef66938cc8d7b3e173217080fe7b55b653
yokarak <ande67f3@edu.easj.dk>
2019-10-04 13:09:15 +0200

    flyttet stensakspapir metoden til StenSaksPapir class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -10,42 +8,9 @@ public class Main {
         Haand anders = Haand.SAKS;
         Haand karten = Haand.STEN;
 
-        // her starter spillet
-        StenSaksPapir runde1 = new StenSaksPapir();
-        System.out.println(runde1.slag(andrács, anders));
-
-
-
-
-
-        //Computer generee en hånd
-        Haand computersHaand;
-        int haandInt = (int) (Math.random()*3);
-        computersHaand = Haand.values()[haandInt];
-        if (haandInt == 0) { computersHaand = Haand.STEN;}
-        if (haandInt == 1) { computersHaand = Haand.SAKS;}
-        if (haandInt == 2) { computersHaand = Haand.PAPIR;}
-        System.out.println("computeren slår " + computersHaand);
-
-        //indløse spillerns hpnd
-        Scanner scanner = new Scanner(System.in);
-        Haand spillerensHaand =null;
-        String spillerensString = scanner.next();
-        if (spillerensString.equalsIgnoreCase("sten")) { spillerensHaand = Haand.STEN;}
-        if (spillerensString.equalsIgnoreCase("saks")) { spillerensHaand = Haand.SAKS;}
-        if (spillerensString.equalsIgnoreCase("papir")) { spillerensHaand = Haand.PAPIR;}
-        System.out.println("Spiller slår " + spillerensHaand);
-
-        // sammenligne med slag() metoden
-        int resultat =runde1.slag(spillerensHaand, computersHaand);
-
-        // udråbe en vinder
-        System.out.println("resultatet er " + resultat);
-        if (resultat ==0) {System.out.println("Uafgjort");}
-        if (resultat==1) { System.out.println("Spiller vandt!");}
-        if (resultat==2) { System.out.println("Spilleren Tabte :(");}
-        if (resultat==-1) { System.out.println("Fejl");}
+        StenSaksPapir.StarSpillet();
 
     }
-    }
+
+}
 
--- src/StenSaksPapir.java
+++ src/StenSaksPapir.java
@@ -1,6 +1,44 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class StenSaksPapir extends Main {
+
+    public static void StarSpillet() {
+        StenSaksPapir runde1 = new StenSaksPapir();
+
+        int count =1;
+        while (count >=1) {
+        //indløse spillerns hånd
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("runde " + count +"nu er det din tur, hvad slår du?");
+        Haand spillerensHaand =null;
+        String spillerensString = scanner.next();
+        if (spillerensString.equalsIgnoreCase("sten")) { spillerensHaand = Haand.STEN;}
+        if (spillerensString.equalsIgnoreCase("saks")) { spillerensHaand = Haand.SAKS;}
+        if (spillerensString.equalsIgnoreCase("papir")) { spillerensHaand = Haand.PAPIR;}
+        System.out.println("Spiller slår " + spillerensHaand);
+
+        // her starter spillet
+
+        //Computer generee en hånd
+        Haand computersHaand;
+        int haandInt = (int) (Math.random()*3);
+        computersHaand = Haand.values()[haandInt];
+        if (haandInt == 0) { computersHaand = Haand.STEN;}
+        if (haandInt == 1) { computersHaand = Haand.SAKS;}
+        if (haandInt == 2) { computersHaand = Haand.PAPIR;}
+        System.out.println("computeren slår " + computersHaand);
+
+        // sammenligne med slag() metoden
+        int resultat =runde1.slag(spillerensHaand, computersHaand);
+
+        // udråbe en vinder
+        System.out.println("resultatet er " + resultat);
+        if (resultat ==0) {System.out.println("Uafgjort");}
+        if (resultat==1) { System.out.println("Spiller vandt!");}
+        if (resultat==2) { System.out.println("Spilleren Tabte :(");}
+        if (resultat==-1) { System.out.println("Fejl");}
+    count++;} }
 
     /**
      * Hvis den første hånd vinder, returneres 1
